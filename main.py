09ebf4b1203b382ac2640b13d270bbf0d51e591d
Sergey Babichev <s.babichevv91p@gmail.com>
2022-07-13 14:31:34 +0300

    change main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
+from account.routes import account_router
+from account.profile_routes import profile_routers
 
 app = FastAPI(title='Coffee app')
+app.include_router(account_router)
+app.include_router(profile_routers)
